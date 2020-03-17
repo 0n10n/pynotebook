d8e69f15fd5554ecee500ee7c123ede1f593351a
0n10n <onion@163.com>
2020-03-17 13:54:22 +0800

    Sending payload

--- 202002/recurse_payload.py
+++ 202002/recurse_payload.py
@@ -1,8 +1,12 @@
-
+#!/usr/bin/python3
 
 import os
+import subprocess
 
-path = input("Enter payload path: ")
+#path = input("Enter payload path: ")
+#path = "/home/danzhu/attack-payloads"
+path = "/home/danzhu/PayloadsAllTheThings-master"
+url = "http://192.168.120.6/dashboard/"
 
 if not os.path.exists(path):
     print('Path must be exist! ', path)
@@ -13,5 +17,10 @@ for root, dirs, files in os.walk(path):
     path = root.split(os.sep)
     #print(os.path.basename(root))
     for name in files:
-        print(os.path.join(root, name))
-        
+        fullname = os.path.join(root, name)
+        args1 = ("wfuzz -c -z file --zP fn='{0}',encoder=urlencode -t 20 --no-cache {1}FUZZ".format(fullname, url))
+        args2 = ("wfuzz -c -z file --zP fn='{0}',encoder=urlencode -t 20 --no-cache {1}?id=FUZZ".format(fullname, url))
+        #subprocess.run(['/usr/bin/wfuzz',args])
+        os.system(args1)
+        os.system(args2)
+        #print(args)
